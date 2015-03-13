b1a503014bbb020d904a1788db77e01fe6a21d99
Carlos Diaz Sagot <Carlos Diaz Sagot@10.68.172.119>
2015-03-13 13:38:39 -0600

    PrimerSubida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author Carlos Diaz Sagot
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //OLIWISSSSSSSSSSSSSSSSSS
+        System.out.println("Proyecto Compartir");
+    }
+    
+}
